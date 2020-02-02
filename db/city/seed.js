9c088c091d06bcc99f9ed201676fb2770c369713
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2020-02-02 14:47:20 -0800

    working in the seed file

---
+++ db/city/seed.js
@@ -0,0 +1,12 @@
+const db = require('../models');
+
+// const movies = require('./movies.json');
+
+// db.Movies.remove({}, () => {
+// 	movies.forEach(movie => {
+// 		db.Movies.create(movie, (error, createdMovie) => {
+// 			if (error) return console.log(error);
+// 			console.log(createdMovie);
+// 		});
+// 	});
+// });
